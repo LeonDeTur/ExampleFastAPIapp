d0b810ef2f5b122d0564f80bf55f1e970d1ca54d
Leon <100690100+LeonDeTur@users.noreply.github.com>
2026-04-02 16:55:59 +0300

    feat(system_service): added service for env setting and env getting

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
+import os
+
 from fastapi import FastAPI
 
 from app.__version__ import APP_VERSION
-from app.test_routers import test_router
+from app.routers.system_router import system_router
+from app.common.config.app_config import FastApiAppConfig
 
-app = FastAPI(version=APP_VERSION, title="Example App", description="Some description")
+app_config = FastApiAppConfig(
+    version=os.environ.get("APP_VERSION"),
+    name=os.environ.get("APP_NAME"),
+    description=os.environ.get("APP_DESCRIPTION")
+)
 
-app.include_router(test_router)
+
+app = FastAPI(
+    title=app_config.name,
+    description=app_config.description,
+    version=app_config.version
+)
 
 
 @app.get("/")
 async def get_root():
     return "root"
+
+app.include_router(system_router)
